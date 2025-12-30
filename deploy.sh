c98e88487e1d3e2197e234291c26c19fc9a4979b
BVPallavan <bvpallavan@gmail.com>
2025-12-30 22:58:56 +0800

    Fix container stop and remove commands in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,21 +5,21 @@ IMAGE_NAME="devops-tasks-app"
 IMAGE_TAG="latest"
 
 # Pull image from registry
-echo "Pulling Docker image: $IMAGE_NAME:$IMAGE_TAG ..."
-docker pull $IMAGE_NAME:$IMAGE_TAG
+#echo "Pulling Docker image: $IMAGE_NAME:$IMAGE_TAG ..."
+#docker pull $IMAGE_NAME:$IMAGE_TAG
 
 # Stop and remove old container if exists
 if docker ps -q -f name=devops-tasks-app-dev; then
 	echo "Stopping devops-tasks-app-dev..."
-	docker stop $IMAGE_NAME || true
-	docker rm $IMAGE_NAME || true
+	docker stop devops-tasks-app-dev || true
+	docker rm devops-tasks-app-dev || true
 fi
 
 # Stop and remove old container if exists
 if docker ps -q -f name=devops-tasks-app-prod; then
 	echo "Stopping devops-tasks-app-prod..."
-	docker stop $IMAGE_NAME || true
-	docker rm $IMAGE_NAME || true
+	docker stop devops-tasks-app-prod || true
+	docker rm devops-tasks-app-prod || true
 fi
 
 # Run new container
