43b44caae22141a597f9c71323d0c97451c76e09
BVPallavan <bvpallavan@gmail.com>
2026-01-01 19:03:56 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,15 +4,15 @@ ENV=$1
 # Stop and remove old container if exists
 if docker ps -q -f name=devops-app-dev; then
 	echo "Stopping devops-app-dev..."
-	docker stop devops-app-dev
-	docker rm devops-app-dev
+	docker stop devops-app-dev || true
+	docker rm devops-app-dev || true
 fi
 
 # Stop and remove old container if exists
 if docker ps -q -f name=devops-app-prod; then
 	echo "Stopping devops-app-prod..."
-	docker stop devops-app-prod
-	docker rm devops-app-prod
+	docker stop devops-app-prod || true
+	docker rm devops-app-prod || true
 fi
 
 # Run new container
